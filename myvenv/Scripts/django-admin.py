221d0ae89ee16dae835c0fcb2ba0b50c9695bb0d
unknown <akawolf13@syuin.ac.kr>
2019-05-23 19:18:38 +0900

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\이명재\desktop\likelion\django\week9\wordcount\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
